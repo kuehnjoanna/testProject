abb2d918918c454af908a28080e6ef9616797220
Joanna Kuehn <kuehnjoanna@gmail.com>
2024-09-23 14:01:35 +0200

    Initial Commit

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Joanna Kühn on 23.09.24.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
